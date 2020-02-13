26a0aa6c6f5f0088bd43ce68b034ded3bd7935a1
Seongjun2 <tjdwns4302@naver.com>
2020-02-13 16:04:19 +0900

    Spring을 이용한 팩토리 패턴

--- SpringTest/src/main/java/Factory/DaoFactory.java
+++ SpringTest/src/main/java/Factory/DaoFactory.java
@@ -4,12 +4,50 @@ import DAO.ConnectionMaker;
 import DAO.DConnectionMaker;
 import DAO.UserDao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
+
 public class DaoFactory {
 
+    /*
     public UserDao userDao(){
         ConnectionMaker connectionMaker = new DConnectionMaker();
         UserDao userDao = new UserDao(connectionMaker);
 
         return userDao;
     }
+    아래 메소드로 변형, 경우에 따라 중복 제거하기 위해
+     */
+
+    @Bean//오브젝트 생성을 담당하는 IoC용 메소드라는 표시
+    public UserDao userDao(){
+//        return new UserDao(new DConnectionMaker());
+        return new UserDao(connectionMaker());
+    }
+
+    /*
+    public AccountDao accountDao(){
+        return new AccountDao(new DConnectionMaker());
+        을
+        return new AccountDao(connectionMaker()); 로 변경
+    }
+
+    public MessageDao messageDao(){
+        return new MessageDao(new DConnectionMaker());
+        을
+        return new MessageDao(connectionMaker()); 로 변경
+    }
+     */
+
+    @Bean
+    //new DConnectionMaker() <<< 중복을 막기 위해 생성된 메서드
+    public ConnectionMaker connectionMaker(){
+        return new DConnectionMaker();
+    }
+
 }
+/*
+ 자바 코드의 탈을 쓰고 있지만 사실은 XML과 같은 스프링 전용 설정정보라고 보는 것이 좋다.
+ */
--- SpringTest/src/test/java/FactoryTest/UserDaoTest.java
+++ SpringTest/src/test/java/FactoryTest/UserDaoTest.java
@@ -2,11 +2,25 @@ package FactoryTest;
 
 import DAO.UserDao;
 import Factory.DaoFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
 
 public class UserDaoTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        /*
         UserDao dao = new DaoFactory().userDao();
         //Client 는 DaoFactory 에 요청 -> DaoFactory 는 UserDao 를 생성, DConnectionMaker 를 생성-> UserDao 는 ConnectionMaker 를 사용
+       */
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        //@Configuration 이 붙은 자바코드를 설정정보로 사용하려면 AnnotationConfigApplicationContext 클래스를 이용
+        //getBean()의 파라미터 userDao 는 메서드 이름
+
+
     }
 }
 
