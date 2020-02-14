d4aa1fb7829b8d037e57deafbffd6ce0cb7867ff
Seongjun2 <tjdwns4302@naver.com>
2020-02-14 20:00:10 +0900

    YBM 코테준비 1차 1급 추가

---
+++ YBMCodingTest/1급/t1/Q2.java
@@ -0,0 +1,106 @@
+// You may use import as below.
+//import java.util.*;
+
+package Daou.Grade1.t1;
+
+public class Q2 {
+    public int solution(int n) {
+        // Write code here.
+        int answer = 1;
+
+        if(n == 1) answer = 1;
+        else if(n == 2) answer = 4;
+        else{
+            int cnt = 0;
+            int std = (n * 1) + (n-2);
+            for (int i = 1; i < n; i++) {
+                answer += n +std;
+                cnt++;
+
+                if(cnt == 2){
+                    std = std - 4;
+                    cnt = 0;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    // The following is main method to output testcase.
+    public static void main(String[] args) {
+        Q2 sol = new Q2();
+        int n1 = 3;
+        int ret1 = sol.solution(n1);
+
+        
+        // Press Run button to receive output. 
+        System.out.println("Solution: return value of the method is " + ret1 + " .");
+        
+        int n2 = 2;
+        int ret2 = sol.solution(n2);
+        
+        // Press Run button to receive output. 
+        System.out.println("Solution: return value of the method is " + ret2 + " .");
+    }
+}
+
+/*
+#문제2
+다음과 같이 n x n 크기의 격자에 1부터 n x n까지의 수가 하나씩 있습니다.
+![image](http://res.cloudinary.com/sgc109/image/upload/c_scale,w_300/v1517462270/%EA%B7%B8%EB%A6%BC1_qysbr6.png)
+이때 수가 다음과 같은 순서로 배치되어있다면 이것을 n-소용돌이 수라고 부릅니다.
+![image](http://res.cloudinary.com/sgc109/image/upload/c_scale,w_300/v1517462270/%EA%B7%B8%EB%A6%BC2_ol8snc.png)
+소용돌이 수에서 1행 1열부터 n 행 n 열까지 대각선상에 존재하는 수들의 합을 구해야 합니다.
+![image](http://res.cloudinary.com/sgc109/image/upload/c_scale,w_300/v1517462270/%EA%B7%B8%EB%A6%BC3_cbcdg3.png)
+위의 예에서 대각선상에 존재하는 수의 합은 15입니다.
+격자의 크기 n이 주어질 때 n-소용돌이 수의 대각선상에 존재하는 수들의 합을 return 하도록 solution 메소드를 완성해주세요.
+
+---
+##### 매개변수 설명
+격자의 크기 n이 solution 메소드의 매개변수로 주어집니다.
+
+* n은 1 이상 100 이하의 자연수입니다.
+
+---
+##### return 값 설명
+n-소용돌이 수의 대각선상에 존재하는 수들의 합을 return 해주세요.
+
+---
+##### 예시
+
+| n 	| return 	|
+|---	|--------	|
+| 3 	| 15     	|
+| 2 	| 4      	|
+
+##### 예시 설명
+예시 #1
+문제의 예와 같습니다.
+
+예시 #2
+![image](http://res.cloudinary.com/sgc109/image/upload/c_scale,w_300/v1517462270/%EA%B7%B8%EB%A6%BC4_astq7q.png)
+1과 3을 더하여 4가 됩니다.
+
+
+
+3 : 1 5 9
+
+4 : 1 7 13 15
+
+5 : 1 9 17 21 25
+
+
+1 2 3 4
+12 13 14 5
+11 16 15 6
+10 9 8 7
+
+
+1 2 3 4 5
+16 17 18 19 6
+15 24 25 20 7
+14 23 22 21 8
+13 12 11 10 9
+
+ */
