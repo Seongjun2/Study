ce7db329ae73ccfd913e6be1bb9a7dcf867b555c
Seongjun2 <tjdwns4302@naver.com>
2020-02-15 20:00:06 +0900

    nothing

--- SpringTest/src/test/java/FactoryTest/UserDaoTest.java
+++ SpringTest/src/test/java/FactoryTest/UserDaoTest.java
@@ -8,8 +8,6 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
