75a1e144c4ad6b2bab898cdc7f97a9ba8088b487
Seongjun2 <tjdwns4302@naver.com>
2020-02-17 20:16:54 +0900

    YBM t3 3문제

---
+++ makeapi/src/test/java/com/example/springrestapi/events/Test.java
@@ -0,0 +1,11 @@
+package com.example.springrestapi.events;
+
+public class Test {
+    public static void main(String[] args) {
+        int[][] a = new int[20][30];
+
+        System.out.println(a.length);
+        System.out.println(a[0].length);
+
+    }
+}
